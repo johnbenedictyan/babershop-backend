a24f01506245add7bb92989ccc38eb92e12973b4
johnbenedictyan <johnyfx00@gmail.com>
2020-07-31 17:17:19 +0800

    Added the functionality for the customer's routes.

--- routes/customer.js
+++ routes/customer.js
@@ -3,18 +3,41 @@ var router = express.Router();
 const mongo = require('../controllers/mongo');
 const ObjectId = require('mongodb').ObjectId
 
+const queue = require('../controllers/queue');
+
 router.get('/view/:barberid', (req, res, next) => {
     const { barberid } = req.params
 });
 
-router.post('/join-queue/:barberid', (req, res, next) => {
-    const { barberid } = req.params
+router.post('/join-queue/:barberid', async(req, res, next) => {
+    const { barberid } = req.params;
+    const { name } = req.body;
+
+    let newEntry = await queue.joinQueue(name,barberid);
+    return res.sendStatus(newEntry.statusCode).json({'message':newEntry.message});
 });
 
 router.get('/leave-queue/:barberid', (req, res, next) => {
     const { barberid } = req.params
+    const uID;
+    // TODO: X509 Cert Processing to check whether there is a valid x509 cert or sign another.
+    // TODO: Extract the uID which would be the cert value
+    // uID = <output of some function>
+
+    let result = await queue.leaveQueue(uID,barberid)
+    return res.sendStatus(newEntry.statusCode).json({ 'message': newEntry.message });
 });
 
 router.get('/view-barbers', (req, res, next) => {
     const { barberid } = req.params
+    const uID;
+    // TODO: X509 Cert Processing to check whether there is a valid x509 cert or sign another.
+    // TODO: Extract the uID which would be the cert value
+    // uID = <output of some function>
+
+    let result = await queue.viewQueue(uID,barberid)
+    return res.sendStatus(newEntry.statusCode).json({
+        'message': newEntry.message,
+        'data': result.data
+    });
 });
