2c0a4c1c50f62a6847f6a684400da21f12f267fe
johnbenedictyan <johnyfx00@gmail.com>
2020-08-01 13:31:02 +0800

    Modified the user controller to include try catch statements as well as integrated with the return object.

--- controllers/users.js
+++ controllers/users.js
@@ -1,62 +1,241 @@
 const mongo = require('../controllers/mongo');
 const ObjectId = require('mongodb').ObjectID;
+const { ReturnObject } = require('./constants');
 
 async function getAll(){
-    let result = await mongo.getDb().collection('barbers').find().toArray();
+    let barbers, result;
+    try {
+        barbers = await mongo.getDb().collection('barbers').find().toArray();
+    } catch (error) {
+        result = new ReturnObject(
+            500,
+            {
+                'message': 'An error has occurred when trying to access all barbers',
+            }
+        )
+    }
+    if (barbers) {
+        result = new ReturnObject(
+            200,
+            {
+                'message': 'You have successfully accessed all barbers',
+                'data': barbers
+            }
+        )
+    } else {
+        result = new ReturnObject(
+            500,
+            {
+                'message': 'An error has occurred when trying to access all barbers',
+            }
+        )
+    }
     return result
 }
 
 async function getUserById(id){
-    let result = await mongo.getDb().collection('barbers').findOne({
-        _id: new ObjectId(id)
-    });
+    let barber, result;
+    try {
+        barber = await mongo.getDb().collection('barbers').findOne({
+            _id: new ObjectId(id)
+        });
+    } catch (error) {
+        result = new ReturnObject(
+            500,
+            {
+                'message': `An error has occurred when trying to access the barber with id #${id}`,
+            }
+        )
+    }
+    if (barber) {
+        result = new ReturnObject(
+            200,
+            {
+                'message': `You have successfully accessed  the barber with id #${id}`,
+                'data': barber
+            }
+        )
+    } else {
+        result = new ReturnObject(
+            500,
+            {
+                'message': `An error has occurred when trying to access the barber with id #${id}`,
+            }
+        )
+    }
     return result
 }
 
 async function getUserByUsername(username){
-    let result = await mongo.getDb().collection('barbers').findOne({
-        username
-    });
-    return
+    let barber, result;
+    try {
+        barber = await mongo.getDb().collection('barbers').findOne({
+            username
+        });
+    } catch (error) {
+        result = new ReturnObject(
+            500,
+            {
+                'message': `An error has occurred when trying to access the barber with username: #${username}`,
+            }
+        )
+    }
+    if (barber) {
+        result = new ReturnObject(
+            200,
+            {
+                'message': `You have successfully accessed  the barber with username: #${username}`,
+                'data': barber
+            }
+        )
+    } else {
+        result = new ReturnObject(
+            500,
+            {
+                'message': `An error has occurred when trying to access the barber with username: #${username}`,
+            }
+        )
+    }
+    return result
 }
 
 async function addUser(username,email,password){
     let user = await getByUserName(username);
+    let newBarber, result;
     if (user == null) {
-        let result = await mongo.getDb().collection('barbers').insertOne({
-            username,
-            email,
-            password
-        })
+        try {
+            newBarber = await mongo.getDb().collection('barbers').insertOne({
+                username,
+                email,
+                password
+            })
+        } catch (error) {
+            result = new ReturnObject(
+                500,
+                {
+                    'message': 'An error has occurred when trying to create a new barber',
+                }
+            )
+        }
+        if (newBarber) {
+            result = new ReturnObject(
+                200,
+                {
+                    'message': 'New barber successfully created',
+                    'data': newbarber
+                }
+            )
+        } else {
+            result = new ReturnObject(
+                500,
+                {
+                    'message': 'An error has occurred when trying to create a new barber',
+                }
+            )
+        }
+    } else {
+        result = new ReturnObject(
+            401,
+            {
+                'message': 'Username has been taken',
+            }
+        )
     }
+    return result
 }
 
 async function updateUser(username, email, password) {
     let user = await getByUserName(username);
+    let updatedBarber, result;
     if (user) {
-        let result = await mongo.getDb().collection('barbers').updateOne({
-            username
-        },
-        {
-            '$set': {
-                username: user.username,
-                email,
-                password
+        try {
+            updatedBarber = await mongo.getDb().collection('barbers').updateOne(
+                { username },
+                {
+                    '$set': {
+                        username: user.username,
+                        email,
+                        password
+                    }
+                }
+            )
+        } catch (error) {
+            result = new ReturnObject(
+                500,
+                {
+                    'message': 'An error has occurred when trying to update barber',
+                }
+            )
+        }
+        if (updatedBarber) {
+            result = new ReturnObject(
+                200,
+                {
+                    'message': 'Successfully updated barber',
+                }
+            )
+        } else {
+            result = new ReturnObject(
+                500,
+                {
+                    'message': 'An error has occurred when trying to update barber',
+                }
+            )
+        }
+    } else {
+        result = new ReturnObject(
+            404,
+            {
+                'message': 'This barber does not exist',
             }
-        })
-        return result
+        )
     }
+    return result
 }
 
 async function deleteUser(id){
     let user = await getUserById(id);
+    let deletedBarber, result;
     if (user) {
-        await mongo.getDb().collection('barbers').deleteOne({
-            // _id: new ObjectId(id)
-            _id: user._id
-        })
+        try {
+            deletedBarber = await mongo.getDb().collection('barbers').deleteOne({
+                // _id: new ObjectId(id)
+                _id: user._id
+            })
+        } catch (error) {
+            result = new ReturnObject(
+                500,
+                {
+                    'message': 'An error has occurred when trying to delete barber',
+                }
+            )
+        }
+        if (deletedBarber) {
+            result = new ReturnObject(
+                200,
+                {
+                    'message': 'Successfully deleted barber',
+                }
+            )
+        } else {
+            result = new ReturnObject(
+                500,
+                {
+                    'message': 'An error has occurred when trying to delete barber',
+                }
+            )
+        }
+    } else {
+        result = new ReturnObject(
+            404,
+            {
+                'message': 'This barber does not exist',
+            }
+        )
     }
+    return result
 }
+
 module.exports = {
     getAll,
     getUserById,
