be80baf7ee55cc73718ce1454a3171bac759e2ed
johnbenedictyan <johnyfx00@gmail.com>
2020-08-12 22:27:38 +0800

    Changed the behaviour of the authenticateToken function so that it will sign a new token if a token is not found.

--- controllers/jwt.js
+++ controllers/jwt.js
@@ -1,4 +1,5 @@
 var jwt = require('jsonwebtoken');
+var uuidv4 = require('uuid/v4');
 
 // TODO: Please change the secret token from plain text to an env var!
 const secretToken = 'tOAl&Qe0%#PIflfYD5&ygHZsbf0ea#uuD0Wzb&*sTnWoPn$*';
@@ -7,7 +8,16 @@ function authenticateToken(req, res, next) {
     // Gather the jwt access token from the request header
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401) // if there isn't any token
+
+    // NOTE: I'm not sure if this is good idea.
+    //       Whether you should just sign a new token if no token is found.
+    //       More research is required.
+    
+    if (token == null) {
+        signToken({
+            customerId: uuidv4()
+        })
+    }
 
     jwt.verify(token, secretToken, (err, decoded) => {
         if (err) return res.sendStatus(403)
